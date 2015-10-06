5a9f1a85974ca3b7eb2b415dea8e4699a44a4c85
naman14 <namandwivedi14@gmail.com>
2015-10-07 02:38:32 +0530

    nav view

---
+++ app/src/main/java/com/greplr/ghumo/MainActivity.java
@@ -0,0 +1,93 @@
+package com.greplr.ghumo;
+
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.greplr.ghumo.fragments.RideFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        setupDrawerContent(navigationView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else  super.onBackPressed();
+
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.getMenu().findItem(R.id.nav_ride).setChecked(true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new RideFragment()).commit();
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(final MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        updatePosition(menuItem);
+                        return true;
+
+                    }
+                });
+    }
+
+    private void updatePosition(final MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_ride:
+                fragment = new RideFragment();
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            final android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction()
+                    .replace(R.id.container, fragment);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    transaction.commit();
+                }
+            }, 350);
+        }
+    }
+}
---
+++ app/src/main/java/com/greplr/ghumo/fragments/RideFragment.java
@@ -0,0 +1,61 @@
+package com.greplr.ghumo.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.greplr.ghumo.R;
+
+/**
+ * Created by naman on 07/10/15.
+ */
+public class RideFragment extends Fragment {
+
+    private GoogleMap mMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_ride, container, false);
+
+        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        final ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setTitle("");
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        addToMap(new LatLng(28,28));
+
+        return rootView;
+    }
+
+    private void addToMap(LatLng latLng){
+
+        MarkerOptions markerOptions;
+        markerOptions = new MarkerOptions();
+
+        markerOptions.position(latLng);
+        mMap.addMarker(markerOptions);
+
+        CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom(latLng, 6.0f);
+
+        mMap.animateCamera(cameraPosition);
+
+    }
+}
